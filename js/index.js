38af227ad7c04bd47f73093b5e8f6e19a1e14b29
Nancy J <156861031+njamison95@users.noreply.github.com>
2025-02-12 23:05:27 -0500

    trying to make expandable

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.body.addEventListener("click", (ev) => {
+	const isExpandableTitle = !!ev.target.closest(".expandable-title-bar");
+	const expandable = ev.target.closest(".expandable");
+
+	if (!isExpandableTitle) {
+		return;
+	}
+
+	expandable.classlist.toggle("expandable-open");
+});
